6843b279014c7c36ac45212450255bd5c2fc5ecf
jyotirmoyiitg-bit <jyotirmoy.iitg@gmail.com>
2025-12-25 19:51:07 +0530

    Initial Commit : 25-12-2025

---
+++ calculator1.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    return a + b
+
+def main():
+    x = 10
+    y = 20
+    print("Sum:", add(x, y))
+
+print("calculator.py file is loaded")
+
+if __name__ == "__main__":
+    main()
---
+++ hello1.py
@@ -0,0 +1,6 @@
+# /D:/github/projects/python_for_ai/hello1.py
+def main():
+    print("Hello, world!")
+
+if __name__ == "__main__":
+    main()
